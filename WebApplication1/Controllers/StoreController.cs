3158588d2eab6f442f9121ea0707e3b8b95aea20
brlch <brlch@hotmail.com>
2017-05-05 00:05:54 -0500

    Se agregaron las vistas de catalogo y carrito

---
+++ WebApplication1/Controllers/StoreController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult Catalog()
+        {
+
+            return View();
+        }
+        public ActionResult Cart()
+        {
+
+            return View();
+        }
+    }
+}
